d5fe13d50ce2ad7f19a8d7cf12166b0c7ce332cd
Lucy Linder <lucy.derlin@gmail.com>
2017-11-12 09:48:44 +0100

    beauty

--- src/pages/business-details/business-details.ts
+++ src/pages/business-details/business-details.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import {Business} from "../../types";
+import {Component} from '@angular/core';
+import {App, NavController, NavParams} from 'ionic-angular';
+import {Business, KeyValuePair} from "../../types";
+import {Page} from "../../Page";
 
 @Component({
   selector: 'page-business-details',
   templateUrl: 'business-details.html',
 })
-export class BusinessDetailsPage {
+export class BusinessDetailsPage extends Page {
 
   private business: Business;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log("HEY");
-    console.log(this.navParams.get('business'));
+  constructor(public navCtrl: NavController, public navParams: NavParams, protected app: App) {
+    super(null, app);
     this.business = this.navParams.get('business');
   }
 
@@ -22,6 +22,13 @@ export class BusinessDetailsPage {
 
   openGMaps(address: string) {
     window.open('https://www.google.ch/maps/search/' + address, '_system');
+  }
+
+  getPromos(b: Business): KeyValuePair[] {
+    if (!b.FCs) return [];
+    let lala =  [].concat.apply([], this.objectToList(b.FCs).map(fc => this.objectToList(fc.promos)));
+    console.log(lala);
+    return lala;
   }
 
 }
